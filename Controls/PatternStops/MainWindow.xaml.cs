ff3dfdbab8ac616395f88fc34cb78c3d3e2a353a
Andrew71 <took77@bk.ru>
2016-01-29 14:36:13 +0300

    111

--- Controls/PatternStops/MainWindow.xaml.cs
+++ Controls/PatternStops/MainWindow.xaml.cs
@@ -1,40 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
+using System.ComponentModel;
+using Lighting.Library;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PatternStops
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            ObservableNotifiableCollection<PatternPoint> pattern = new ObservableNotifiableCollection<PatternPoint>();
+            pattern.Add(new PatternPoint(Colors.Blue, 2));
+            pattern.Add(new PatternPoint(Colors.Red, 254));
+            pattern.Add(new PatternPoint(Colors.Yellow, 607));
+            pattern.Add(new PatternPoint(Colors.Green, 903));
+            Pattern = pattern;
         }
 
-        private void MultiSlider_SliderSet(object sender, Xam.Wpf.Controls.MultiSliderSetRoutedEventArgs e)
+        private ObservableNotifiableCollection<PatternPoint> _pattern;
+        public ObservableNotifiableCollection<PatternPoint> Pattern
         {
-            if (listBox != null)
-                listBox.ItemsSource = e.SliderValues;
+            get { return _pattern; }
+            set { if (value != _pattern) _pattern = value; OnPropertyChanged("Pattern"); }
         }
 
-        private void MultiSlider_ValueChanged(object sender, Xam.Wpf.Controls.MultiSliderRoutedEventArgs e)
+        private PatternPoint _selectedPoint;
+
+        public PatternPoint SelectedPoint
         {
-            //position.Text = e.Position.ToString();
-            position.Text = ((int)e.SliderValues[e.Position]).ToString();
+            get { return _selectedPoint; }
+            set { if (_selectedPoint != value ) _selectedPoint = value; OnPropertyChanged("SelectedPoint"); }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _selectedPatternIx = -1;
+
+        public int SelectedPatternIx
+        {
+            get { return _selectedPatternIx; }
+            set { if ( _selectedPatternIx != value ) _selectedPatternIx = value; OnPropertyChanged("SelectedPatternIx"); }
+        }
+
     }
 }
